0240da6a3cea6192a0e8ff7888851ffaefedc36b
GuidoMartinez <guidojmartinez@gmail.com>
2023-09-01 03:36:20 +0000

    config iniciales + script creacion ./obj

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+CWD=$PWD
+cd ../cpu/
+echo "\nCreando carpetas en: ${PWD}"
+mkdir obj
+cd ../kernel/
+echo "\nCreando carpetas en: ${PWD}"
+mkdir obj
+cd ../memoria/
+echo "\nCreando carpetas en: ${PWD}"
+mkdir obj
+cd ../filesystem/
+echo "\nCreando carpetas en: ${PWD}"
+mkdir obj
+cd ../shared/
+echo "\nCreando carpetas en: ${PWD}"
+mkdir obj
+clear
+echo "\n\t\t INSTALACION FINALIZAD\n"
