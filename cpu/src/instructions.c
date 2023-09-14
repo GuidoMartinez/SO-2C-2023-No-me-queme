fe93aa35895423d3562472e7adff2ef5cbfff043
Ramiro <ramamonaco98@gmail.com>
2023-09-14 21:35:12 +0000

    Actualizo Instrucciones

--- cpu/src/instructions.c
+++ cpu/src/instructions.c
@@ -1,13 +1,13 @@
 #include "instructions.h"
 
 // Asigna al registro el valor pasado como parámetro.
-void _set(char *registro, char* valor)
+void _set(char *registro, char* valor, t_contexto_ejecucion *contexto)
 {
     *(get_registry(registro)) = str_to_uint32(valor);
 }
 
 // Suma al registro el valor pasado como parámetro.
-void _sum(char *registro_destino, char *registro_origen)
+void _sum(char *registro_destino, char *registro_origen, t_contexto_ejecucion *contexto)
 {
     uint32_t *destino = malloc(sizeof(uint32_t));
     destino = get_registry(registro_destino);
@@ -21,7 +21,7 @@ void _sum(char *registro_destino, char *registro_origen)
 }
 
 // Resta al registro el valor pasado como parámetro.
-void _sub(char *registro_destino, char *registro_origen)
+void _sub(char *registro_destino, char *registro_origen, t_contexto_ejecucion *contexto)
 {
     uint32_t *destino = malloc(sizeof(uint32_t));
     destino = get_registry(registro_destino);
@@ -35,7 +35,7 @@ void _sub(char *registro_destino, char *registro_origen)
 }
 
 // Si valor del registro != cero, actualiza el IP al número de instrucción pasada por parámetro.
-void _jnz(char *registro, char* instruccion)
+void _jnz(char *registro, char* instruccion, t_contexto_ejecucion *contexto)
 {
     uint32_t *regis = malloc(sizeof(uint32_t));
     regis = get_registry(registro);
@@ -51,25 +51,25 @@ void _jnz(char *registro, char* instruccion)
 
 // Syscall bloqueante. Devuelve el Contexto de Ejecución actualizado al Kernel
 // junto a la cantidad de segundos que va a bloquearse el proceso.
-void _sleep(char* tiempo)
+void _sleep(char* tiempo, t_contexto_ejecucion *contexto)
 {
     //devolver pcb al kernel
 }
 
 // Esta instrucción solicita al Kernel que se asigne una instancia del recurso indicado por parámetro.
-void _wait(char *recurso)
+void _wait(char *recurso, t_contexto_ejecucion *contexto)
 {
     //solicitar al kernel asignar recurso y que actualice su contexto
 }
 
 //  Esta instrucción solicita al Kernel que se libere una instancia del recurso indicado por parámetro.
-void _signal(char *recurso)
+void _signal(char *recurso, t_contexto_ejecucion *contexto)
 {
     //solicitar al kernel asignar recurso y que actualice su contexto
 }
 
 // Lee el valor de memoria correspondiente a la Dirección Lógica y lo almacena en el Registro.
-void _mov_in(char *registro, char* direc_logica)
+void _mov_in(char *registro, char* direc_logica, t_contexto_ejecucion *contexto)
 {
     uint32_t *regis = malloc(sizeof(uint32_t));
     regis = get_registry(registro);
@@ -81,7 +81,7 @@ void _mov_in(char *registro, char* direc_logica)
 
 // Lee el valor del Registro y lo escribe en la dirección
 // física de memoria obtenida a partir de la Dirección Lógica.
-void _mov_out(char* direc_logica, char *registro)
+void _mov_out(char* direc_logica, char *registro, t_contexto_ejecucion *contexto)
 {
     uint32_t *regis = malloc(sizeof(uint32_t));
     regis = get_registry(registro);
@@ -91,34 +91,34 @@ void _mov_out(char* direc_logica, char *registro)
 }
 
 // Solicita al kernel que abra el archivo pasado por parámetro con el modo de apertura indicado.
-void _f_open(char *nombre_archivo, char *modo_apertura)
+void _f_open(char *nombre_archivo, char *modo_apertura, t_contexto_ejecucion *contexto)
 {
 }
 
 // Solicita al kernel que cierre el archivo pasado por parámetro.
-void _f_close(char *nombre_archivo)
+void _f_close(char *nombre_archivo, t_contexto_ejecucion *contexto)
 {
 }
 
 // Solicita al kernel actualizar el puntero del archivo a la posición pasada por parámetro.
-void _f_seek(char *nombre_archivo, char* posicion)
+void _f_seek(char *nombre_archivo, char* posicion, t_contexto_ejecucion *contexto)
 {
 }
 
 // Solicita al Kernel que se lea del archivo indicado y
 // se escriba en la dirección física de Memoria la información leída
-void _f_read(char *nombre_archivo, char* direc_logica)
+void _f_read(char *nombre_archivo, char* direc_logica, t_contexto_ejecucion *contexto)
 {
 }
 
 // Solicita al Kernel que se escriba en el archivo indicado l
 // la información que es obtenida a partir de la dirección física de Memoria.
-void _f_write(char *nombre_archivo, char* direc_logica)
+void _f_write(char *nombre_archivo, char* direc_logica, t_contexto_ejecucion *contexto)
 {
 }
 
 //  Solicita al Kernel que se modifique el tamaño del archivo al indicado por parámetro.
-void _f_truncate(char *nombre_archivo, char* tamanio)
+void _f_truncate(char *nombre_archivo, char* tamanio, t_contexto_ejecucion *contexto)
 {
 }
 
